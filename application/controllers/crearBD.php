047af268e55d280c8c84374b4eb92713e8820e16
mmarinero <mmarinero@7aed2805-7aa7-4200-ac04-144bf2236312>
2012-06-11 13:40:14 +0000

    commit parcial desde el portatil, crearDB esta a medias

--- application/controllers/crearBD.php
+++ application/controllers/crearBD.php
@@ -4,23 +4,34 @@
 //los datos para crear cada tabla se definen en su modelo correspondiente y aqui se leen los datos de los modelos listados en 
 //el array
 //Para la el acceso a la base de datos se debe usar la clase DB_Forge
-class CrearDB extends CI_Controller
+class CrearBD extends CI_Controller
 { 
 	//check table exists information_schema
 	//exec mysql < script
 	//ICustomType fuera de $fields  los elementos
 	//refs array clase ref
-        private $models = array('Trabajador','PlanFases','PlanIteraccion','TareaPersonal', 'Actividad');
+	
+    private $models = array('Trabajador','PlanFases','PlanIteraccion','TareaPersonal', 'Actividad');
+	
 	function __construct()
 	{
 	    parent::__construct();
             $this->load->dbforge();
 	}
 
-	public function crear() {
-	    $this->db->query("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 't1')"); 
-	    if (!$table_exists) {
-		exec("mysql -u ".$user." -p ".$password." < ".$APPATH."/helpers/schema.sql");
+    public function crear() {
+	//$this->db->query("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 't1')");
+	echo exec("mysql grupo10 --user=".$this->db->username." --password=".$this->db->password." < ".APPPATH."helpers/schema.sql 2>&1");
+	foreach ($this->models as $model){
+	    $this->load->model($model);
+	    foreach($this->$model->getFields() $field) {
+		$ciFields[] = $field->getCIDBSql()
 	    }
+	    $ciFields[] = array('id'=>array('type'=>'int', 'constraint'=>'8'));
+	    $ciFields[] = array('hashId'=>array('type'=>'varchar', 'constraint'=>'32'));
+	    $ciFields[] = array('created'=>array('type'=>'timestamp', 'constraint'=>'8'));
+	    $ciFields[] = array('updated'=>array('type'=>'timestamp', 'constraint'=>'8'));
+	    $this->dbforge->addField($ciFields);
+
 	}
 }
