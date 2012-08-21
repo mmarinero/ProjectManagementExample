869862731b10aa0c0bbd6a3494b0c3d876373c88
mmarinero <mmarinero@7aed2805-7aa7-4200-ac04-144bf2236312>
2012-08-21 21:22:54 +0000

    create site script mostly finished, hostnames and testing pending

--- application/helpers/apacheMysqlCreateSite.bash
+++ application/helpers/apacheMysqlCreateSite.bash
@@ -3,11 +3,16 @@
 EXPECTED_ARGS=2
 E_BADARGS=65
 MYSQL=`which mysql`
-USERNAME = $1
-PASS = $2
+name = $1
+pass = $2
 document_root = "public_html"
+root_dir = "$HOME/vhosts/"
+server_user = "www-data"
+domain = "$1.local"
+email = "mariomarinero@gmail.com"
+
 Q1="CREATE DATABASE $1;"
-Q2="GRANT ALL ON $USERNAME.* TO '$USERNAME.'@'localhost' IDENTIFIED BY '$PASS';"
+Q2="GRANT ALL ON $name.* TO '$name.'@'localhost' IDENTIFIED BY '$pass';"
 Q3="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}"
  
@@ -22,33 +27,29 @@ $MYSQL -uroot -p -e "$SQL"
 
 # This script is used to create virtual hosts.
 
-dir = "$HOME/vhosts/$USERNAME/"
-domain = "$1.local"
-email = "mariomarinero@gmail.com"
-
 # Create the web directory and a index.php test file
+dir = "$root_dir/$name/"
 mkdir -p "$dir/$document_root"
 cd "$dir/$document_root"
 
 # Set the owner and change permissions
-chown -R ftpuser:www-data $homedir/
-chown -R ftpuser:www-data $homedir/$sn/
+chown -R $server_user $dir/
 chmod -R '750' $dir
 
 # Create a directory for your apache errors log
-mkdir /var/log/apache2/$USERNAME/
+mkdir /var/log/apache2/$name/
 
 # Creation the file with VirtualHost configuration in /etc/apache2/site-available/
 echo "<VirtualHost *:80>
-        ServerAdmin $se
-        ServerName $sn
-        ServerAlias www.$sn
-        DocumentRoot $homedir/$sn/public_html/
+        ServerAdmin $email
+        ServerName $domain
+        ServerAlias www.$domain
+        DocumentRoot $dir/$document_root
         <Directory />
                 Options FollowSymLinks
                 AllowOverride All
         </Directory>
-        <Directory $homedir/$sn/public_html/>
+        <Directory $dir/$document_root>
                 Options Indexes FollowSymLinks MultiViews
                 AllowOverride All
                 Order allow,deny
@@ -61,11 +62,11 @@ echo "<VirtualHost *:80>
                 Order allow,deny
                 Allow from all
         </Directory>
-        ErrorLog /var/log/apache2/$sn/error.log
+        ErrorLog /var/log/apache2/$name/error.log
         # Possible values include: debug, info, notice, warn, error, crit,
         # alert, emerg.
         LogLevel warn
-        CustomLog /var/log/apache2/$sn/access.log combined
+        CustomLog /var/log/apache2/$name/access.log combined
     Alias /doc/ "'/usr/share/doc/'"
     <Directory "'/usr/share/doc/'">
         Options Indexes MultiViews FollowSymLinks
@@ -74,13 +75,13 @@ echo "<VirtualHost *:80>
         Deny from all
         Allow from 127.0.0.0/255.0.0.0 ::1/128
     </Directory>
-</VirtualHost>" > /etc/apache2/sites-available/$sn
+</VirtualHost>" > /etc/apache2/sites-available/$name
 
 # Add the host to the hosts file
-echo 127.0.0.1 $sn >> /etc/hosts
+#echo 127.0.0.1 $sn >> /etc/hosts
 
 # Enable the site
-a2ensite $sn
+a2ensite $name
 
 # Reload Apache2
 /etc/init.d/apache2 reload
